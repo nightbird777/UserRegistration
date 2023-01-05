132ce2f4b3db128d0e7cb0b94a4ab1cb97e083b6
nyctotampa@outlook.com <nyctotampa@outlook.com>
2023-01-05 15:11:22 -0500

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserRegistration.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.IO;
+
+namespace UserRegistration.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User user, HttpPostedFileBase file)
+        {
+            string mainConn = ConfigurationManager.ConnectionStrings["MyConnection"].ConnectionString;
+            SqlConnection connection= new SqlConnection(mainConn);
+            string sql = "insert into [dbo].[Registration] (FirstName, LastName, Image) values (@FirstName, @LastName, @Image)";
+            SqlCommand cmd= new SqlCommand(sql, connection);
+            connection.Open();
+            cmd.Parameters.AddWithValue("@FirstName", user.FirstName);
+            cmd.Parameters.AddWithValue("@LastName", user.LastName);
+            
+            if (file != null && file.ContentLength > 0)
+            {
+                string fileName = Path.GetFileName(file.FileName);
+                string imgPath = Path.Combine(Server.MapPath("~/App_Data/Image/"), fileName);
+                file.SaveAs(imgPath);
+            }
+            cmd.Parameters.AddWithValue("@Image", "~/App_Data/Image/" + file.FileName);
+            cmd.ExecuteNonQuery();
+            connection.Close();
+            ViewData["Message"] = "User Record " + user.FirstName + " " + user.LastName + " is saved Successfully!!!";
+            return View();
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserRegistration.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "Enter First Name: ")]
+        [Display(Name = "Enter First Name: ")]
+        [StringLength(maximumLength:15, MinimumLength = 3, ErrorMessage = "First Name must be between 3 and 15 character long!")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Enter Last Name: ")]
+        [Display(Name = "Enter Last Name: ")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Upload Profile Photos: ")]
+        [Display(Name = "Profile Picture: ")]
+        public string Image { get; set; }
+    }
+}
